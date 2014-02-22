85207d3dfc3fcbb87db818fdb1d68ab7482172a5
Joan G Florit <joan.g.florit@gmail.com>
2014-02-22 04:38:52 +0100

    documentation updated

--- Android/src/com/joanflo/utils/AssetsUtils.java
+++ Android/src/com/joanflo/utils/AssetsUtils.java
@@ -14,7 +14,7 @@ public class AssetsUtils {
 	
 	public static final String BADGES_DIRECTORY = "badges/";
 	public static final String BRANDS_DIRECTORY = "brands/";
-	public static final String FLAGS_DIRECTORY = "flags/";
+	public static final String NFC_DIRECTORY = "nfc/";
 	public static final String CATEGORIES_DIRECTORY = "categories/";
 	
 	
